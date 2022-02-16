59358d1c777f0de2a837c8742c2fbfaedbde5632
nniks19 <77620949+nniks19@users.noreply.github.com>
2022-02-16 19:57:52 +0100

    -

--- json_to_csv.py
+++ json_to_csv.py
@@ -3,4 +3,4 @@ import glob
 
 for filename in glob.glob("data/*.json"):
     df = pd.read_json(r'' + filename)
-    df.to_csv(r'converted/' + filename.replace("data/","").replace(".json",".csv"), header=False)
+    df.to_csv(r'converted/' + filename.replace("data/","").replace(".json",".csv"), header=None, index=False)
