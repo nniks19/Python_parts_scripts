3c036f50e79427bd479eec03632da98cbd276f97
nniks19 <77620949+nniks19@users.noreply.github.com>
2022-02-16 18:24:01 +0100

    v2
    list of json files
    json to csv converter

---
+++ all_json_files.py
@@ -0,0 +1,4 @@
+# All files and directories ending with .json and that don't begin with a dot:
+import glob
+
+print(glob.glob("data/*.json"));
---
+++ json_to_csv.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import glob
+
+for filename in glob.glob("data/*.json"):
+    df = pd.read_json(r'' + filename)
+    df.to_csv(r'converted/' + filename.replace("data/","").replace(".json",".csv"), header=False)
